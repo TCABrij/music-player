a271fdfb99a39621b4875cf7022e5249f269d318
Brij Kishor <exreply7878@gmail.com>
2022-08-04 19:43:39 +0530

    minor changes

---
+++ sasta-app.js
@@ -0,0 +1,28 @@
+const audio = document.querySelector('audio')
+const audioSrc = document.querySelector('source')
+const nextBtn = document.querySelector('.next-btn')
+const previousBtn = document.querySelector('.previous-btn')
+
+playBtn.addEventListener('click', playMusic)
+nextBtn.addEventListener('click', playNext)
+
+function playMusic(){
+
+    if(audio.dataset.paused == 'true')
+    {
+        audio.play()
+        audio.dataset.paused = 'false'
+    }
+    else
+    {
+        audio.pause()
+        audio.dataset.paused = 'true'   
+    }
+    console.log(audio.dataset.paused)
+}
+
+function playNext(){
+    audioSrc.src = '/src/audio/Alan Walker - Darkside .m4a'
+    audio.load()
+    audio.play()
+}
