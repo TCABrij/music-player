c91bac18263bef7ed25b5c72b07583871165eb49
Brij Kishor <exreply7878@gmail.com>
2022-09-19 22:05:53 +0530

    add tracks

--- sasta-app.js
+++ sasta-app.js
@@ -8,11 +8,52 @@ const audioSrc = document.querySelector('source')
 const nextBtn = document.querySelector('.next-btn')
 const previousBtn = document.querySelector('.previous-btn')
 
+// Player Object
+const player = {
+    track_name: '',
+    track_artist: '',
+    track_thumbnail: '',
+    track_src: '',
+    track_duration: 0,
+    track_current_progress: 0,
+    updateDOM: function(){
+        document.querySelector('.art').src = this.track_thumbnail
+        document.querySelector('marquee').textContent = this.track_name
+        document.querySelector('h3.artist').textContent = this.track_artist
+
+    },
+    audioPlayer: function(){
+        audioSrc.src = this.track_src
+    }
+}
+
 playBtn.addEventListener('click', playMusic)
 nextBtn.addEventListener('click', playNext)
 
-function playMusic(){
+// Load Track to Player Object
+document.addEventListener('DOMContentLoaded', loadTracks)
 
+
+function loadTracks(){
+    
+    // Get a track info object
+    let track = getTrack()
+    player.track_name = track.name
+    player.track_artist = track.artist
+    player.track_thumbnail = track.thumbnail
+    console.log(player.track_thumbnail)
+    player.track_src = track.src
+
+    // update Player DOM and Audio Player
+    player.updateDOM()
+    player.audioPlayer()
+}
+
+function getTrack(){
+    return tracks[0]
+}
+
+function playMusic(){
     if(audio.dataset.paused == 'true')
     {
         audio.play()
@@ -27,7 +68,10 @@ function playMusic(){
 }
 
 function playNext(){
-    audioSrc.src = '/src/audio/Alan Walker - Darkside .m4a'
+    audioSrc.src = '/src/audio/3.m4a'
     audio.load()
     audio.play()
 }
+
+
+
---
+++ src/audio/tracks-info.js
@@ -0,0 +1,93 @@
+const tracks = [
+    {
+        id: 1,
+        name: 'Alan Walker - Alone',
+        artist: 'Alan Walker',
+        thumbnail: 'src/img/track-thumbs/alanwalker.webp',
+        src: '/src/audio/1.m4a'
+    },
+    {
+        id: 2,
+        name: 'Alan Walker - Darkside',
+        artist: 'Alan Walker',
+        thumbnail: 'src/img/track-thumbs/alanwalker.webp',
+        src: 'src/audio/2.m4a'
+    },
+    {
+        id: 3,
+        name: 'Alan Walker - Play',
+        artist: 'Alan Walker',
+        thumbnail: 'src/img/track-thumbs/alanwalker.webp',
+        src: 'src/audio/3.m4a'
+    },
+    {
+        id: 4,
+        name: 'Lily',
+        artist: 'Alan Walker, K-391 & Emelie Hollow',
+        thumbnail: 'src/img/track-thumbs/alanwalker.webp',
+        src: 'src/audio/4.m4a'
+    },
+    {
+        id: 5,
+        name: 'Alan Walker - The Spectre',
+        artist: 'Alan Walker',
+        thumbnail: '../img/track-thumbs/alanwalker.webp',
+        src: '5.m4a'
+    },
+    {
+        id: 6,
+        name: 'Despacito - Indian Classic Version',
+        artist: 'Praveen Prathapan & Janan',
+        thumbnail: 'src/img/track-thumbs/despacito.jpg',
+        src: 'src/audio/6.mp3'
+    },
+    {
+        id: 7,
+        name: 'Apollo 69',
+        artist: 'Goblin from Mars',
+        thumbnail: 'src/img/track-thumbs/goblin-from-mars.jpg',
+        src: 'src/audio/7.mp3'
+    },
+    {
+        id: 8,
+        name: 'Satisfaya',
+        artist: 'Imran Khan',
+        thumbnail: 'src/img/track-thumbs/imran.jpg',
+        src: 'src/audio/8.m4a'
+    },
+    {
+        id: 9,
+        name: 'Invincible',
+        artist: 'NoCopyrightSounds[NCS]',
+        thumbnail: 'src/img/track-thumbs/ncs.jpg',
+        src: 'src/audio/9.mp3'
+    },
+    {
+        id: 10,
+        name: 'Ignite - Live Performance',
+        artist: 'Alan Walker & K-391',
+        thumbnail: 'src/img/track-thumbs/alanwalker.webp',
+        src: 'src/audio/10.m4a'
+    },
+    {
+        id: 11,
+        name: 'Fight Back',
+        artist: 'NEFFEX',
+        thumbnail: 'src/img/track-thumbs/neffex.jpg',
+        src: 'src/audio/11.m4a'
+    },
+    {
+        id: 12,
+        name: 'Never Give Up',
+        artist: 'NEFFEX',
+        thumbnail: 'src/img/track-thumbs/neffex.jpg',
+        src: 'src/audio/12.m4a'
+    },
+    {
+        id: 13,
+        name: 'Watch me',
+        artist: 'NEFFEX',
+        thumbnail: 'src/img/track-thumbs/neffex.jpg',
+        src: 'src/audio/13.m4a'
+    }
+]
